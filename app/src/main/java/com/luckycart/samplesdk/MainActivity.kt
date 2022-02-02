decad8a689a34a794f992d1c12f40f1c041a6582
HaythemSouissi <suissi.haythem@gmail.com>
2022-02-02 15:23:21 +0100

    call listAvailableBanners() in the sample

--- app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
+++ app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
@@ -2,17 +2,29 @@ package com.luckycart.samplesdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.luckycart.model.Banners
 import com.luckycart.model.LCAuthorization
 import com.luckycart.sdk.LuckCartSDK
+import com.luckycart.sdk.LuckyCartListenerCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LuckyCartListenerCallback {
+    var luckyCartSDK: LuckCartSDK? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val auth = LCAuthorization(AUTH_KEY, "")
-        LuckCartSDK(this).init(auth, CUSTOMER_ID)
-        LuckCartSDK(this).setUser(CUSTOMER_ID)
+        luckyCartSDK = LuckCartSDK(this)
+        luckyCartSDK?.init(auth, CUSTOMER_ID)
+        luckyCartSDK?.setUser(CUSTOMER_ID)
+        luckyCartSDK?.setActionListener(this)
+        luckyCartSDK?.listAvailableBanners()
     }
+
+    override fun listAvailableBanners(banners: Banners) {
+        Log.d("listAvailableBanners", "" + banners)
+    }
+
 
 }
