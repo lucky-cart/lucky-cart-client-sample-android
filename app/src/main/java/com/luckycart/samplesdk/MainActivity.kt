aaec0e07ccf2c028553586aaa8fe29bcbd4007c6
HaythemSouissi <suissi.haythem@gmail.com>
2022-02-02 01:14:48 +0100

    use sdk folder

--- app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
+++ app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
@@ -2,10 +2,15 @@ package com.luckycart.samplesdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luckycart.sdk.LuckCartSDK
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LuckCartSDK().init()
     }
+
 }
