81a9896658560efdd079423c21818a974be86036
HaythemSouissi <suissi.haythem@gmail.com>
2022-02-02 11:38:38 +0100

    init sdk / set user

---
+++ app/src/main/java/com/luckycart/samplesdk/Constant.kt
@@ -0,0 +1,4 @@
+package com.luckycart.samplesdk
+
+const val AUTH_KEY = "ugjArgGw"
+const val CUSTOMER_ID = "41410788"
--- app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
+++ app/src/main/java/com/luckycart/samplesdk/MainActivity.kt
@@ -2,6 +2,7 @@ package com.luckycart.samplesdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luckycart.model.LCAuthorization
 import com.luckycart.sdk.LuckCartSDK
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        LuckCartSDK().init()
+        val auth = LCAuthorization(AUTH_KEY, "")
+        LuckCartSDK(this).init(auth, CUSTOMER_ID)
+        LuckCartSDK(this).setUser(CUSTOMER_ID)
     }
 
 }
